57ae85b4ee89ed11a2c5478e883056aa1f4b5bff
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-03-02 19:59:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
-
-
 #include "funcs.h"
 
-int main()
-{
-  return 0;
+int main() {
+    std::cout << "Is divisible? \n";
+    std::cout << "isDivisibleBy(100, 25)\n";
+    std::cout << std::boolalpha << isDivisibleBy(100, 25) << "\n";
+    std::cout << "isDivisibleBy(35, 17)\n";
+    std::cout << std::boolalpha << isDivisibleBy(35, 17) << "\n";
+    std::cout << "\n--------------\n";
+
+    std::cout << "Is a prime? \n";
+    std::cout << "isPrime(11)\n";
+    std::cout << isPrime(11) << "\n";
+    std::cout << "\n--------------\n";
+
+    std::cout << "Next prime? \n";
+    std::cout << "nextPrime(14)\n";
+    std::cout << nextPrime(14) << "\n";
+    std::cout << "\n--------------\n";
+
+    std::cout << "Count prime? \n";
+    std::cout << "countPrimes(11, 17)\n";
+    std::cout << countPrimes(11, 17) << "\n";
+    std::cout << "\n--------------\n";
+
+    std::cout << "Is twin prime? \n";
+    std::cout << "isTwinPrime(13)\n";
+    std::cout << isTwinPrime(13) << "\n";
+    std::cout << "\n--------------\n";
+
+    std::cout << "Next twin prime? \n";
+    std::cout << "nextTwinPrime(13)\n";
+    std::cout << nextTwinPrime(13) << "\n";
+    std::cout << "\n--------------\n";
+
+    std::cout << "Largest twin prime? \n";
+    std::cout << "largestTwinPrime(13)\n";
+    std::cout << largestTwinPrime(5, 18) << "\n";
+    std::cout << "\n--------------\n";
+
+    return 0;
 }
